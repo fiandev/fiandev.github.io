8393032e228d4223605c669a4df44cb4c6af3578
fiandev <98071117+fiandev@users.noreply.github.com>
2022-02-02 13:57:22 +0700

    Add files via upload

--- game/shooter-ball/main.js
+++ game/shooter-ball/main.js
@@ -1,7 +1,10 @@
 const canvas = document.querySelector("#canvas"),
 c = canvas.getContext("2d"),
-score_result = document.querySelector("#score"),
-lastScore = localStorage.getItem("lastScore", score);
+score_result = document.querySelector("#score")
+bigScore = document.querySelector("#bigScore"),
+popUp = document.querySelector(".popUp"),
+lastScore = localStorage.getItem("lastScore", score)
+scoreboard = document.querySelector(".scoreboard");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 var bg_audio = new Audio()
@@ -22,7 +25,7 @@ class Player {
     c.fill()
   }
 }
-
+const friction = 0.99
 class Projectile {
   constructor(x , y, radius, color, velocity) {
     this.x = x,
@@ -40,8 +43,36 @@ class Projectile {
   
   update() {
     this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
     this.x = this.x + this.velocity.x
     this.y = this.y + this.velocity.y
+  }
+}
+class Particel {
+  constructor(x , y, radius, color, velocity) {
+    this.x = x,
+    this.y = y,
+    this.radius = radius,
+    this.color = color,
+    this.velocity = velocity,
+    this.alpha = 1
+  }
+  draw() {
+    c.save()
+    c.globalAlpha = 0.1
+    c.beginPath()
+    c.arc(this.x , this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.restore()
+  }
+  
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
   }
 }
 
@@ -70,50 +101,22 @@ class Enemy {
 
 const x = canvas.width / 2
 const y = canvas.height / 2
-const player = new Player(x,y,10,"white")
-
-/*
-const projectile = new Projectile(
-     canvas.width / 2,
-     canvas.height / 2,
-     5,
-     "red",
-     {
-       x: 1,
-       y: 1
-     }
-  )
-  */
-  
-const projectiles = []
-const enemies = []
+let player = new Player(999,999,10,"white")
+let projectiles = []
+let enemies = []
+let particels = []
 var animationId;
 var score = 0;
+let opening, ending
 if (lastScore != "undefined") {
   score = parseInt(lastScore)
+  bigScore.innerHTML = lastScore
 }
-addEventListener("click", (event) => {
-  const angle = Math.atan2(
-    event.clientY - canvas.height / 2,
-    event.clientX - canvas.width / 2
-    )
-  //const speed = Math.floor(Math.random() * (6 - 4) + 4)
-  const speed = 5
-  const rad = Math.floor(Math.random() * (15 - 5) + 5)
-  const velocity = {
-    x: Math.cos(angle) * speed,
-    y: Math.sin(angle) * speed
-  }
-  const color = `hsl(${Math.random() * 360}, 50%, 50%)`
-  projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, rad, color, velocity)
-  )
-  
-})
+
 
 function spawnEnemies() {
-  let cdSpawn = Math.floor(Math.random() * (1500 - 100) + 100)
   //console.log(cdSpawn)
-  setTimeout(() => {
+  setInterval(() => {
     const radius = Math.random() * (30 - 4) + 4
     let x,y
     if (Math.random() < 0.5) {
@@ -128,23 +131,30 @@ function spawnEnemies() {
     canvas.height / 2 - y,
     canvas.width / 2 - x
     )
-    const speed = Math.floor(Math.random() * (6 - 3) + 3)
+    //const speed = Math.floor(Math.random() * (5 - 3) + 3)
+    const speed = 1
     const velocity = {
      x: Math.cos(angle) * speed,
      y: Math.sin(angle) * speed
     }
-    const point = Math.floor(Math.random() * (3 - 1) + 1)
+    const point = Math.floor(Math.random() * (150 - 100) + 100)
     enemies.push(new Enemy(x,y,radius,color,velocity, point))
-    spawnEnemies();
   },1000)
 }
 function animate(){
   score_result.innerHTML = score;
-  bg_audio.play();
+  bigScore.innerHTML = score;
+ // bg_audio.play();
   animationId = requestAnimationFrame(animate)
   c.fillStyle = "rgba(0, 0, 0, 0.1)"
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
+  particels.forEach((particel, index) => {
+    if (particel.alpha <= 0) {
+      particels.splice(index, 1)
+    }
+    particel.update()
+  })
   projectiles.forEach((projectile,index) => {
     projectile.update()
     /* clear projectiles edges screen */
@@ -165,6 +175,7 @@ function animate(){
     /* game over */
     if (dist - enemy.radius - player.radius < 1) {
       //console.log(`game over !`);
+      scoreboard.style.display="none"
       bg_audio.pause()
       var audio = new Audio()
           audio.src = "dead.wav"
@@ -172,49 +183,109 @@ function animate(){
           
       cancelAnimationFrame(animationId)
       score_result.innerHTML = `Game Over!`;
-      document.querySelector(".scoreboard").classList.add("game-over")
+      
       localStorage.setItem("lastScore", score)
-      let body = document.querySelector("body")
-      let element = `
-      <div class="popUp">
-      <p>score : ${score}</p>
-      <p style="margin-bottom:10px;">try again?</p>
-      <button onclick="no()" id="no">no</button>
-      <button onclick="yes()" id="yes">yes</button>
-      </div>`;
-      body.insertAdjacentHTML("beforeend", element)
+      popUp.style.display="block"
     }
     projectiles.forEach((projectile, projectileIndex) => {
     const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
     /* object touch */
       if (dist - enemy.radius - projectile.radius < 1) {
-        
+        for (let i = 0; i < enemy.radius; i++) {
+          particels.push(new Particel(projectile.x, projectile.y, 3 , enemy.color , {
+          x: (Math.random() - 0.5) * (Math.random() * 5),
+          y: (Math.random() - 0.5) * (Math.random() * 5)
+        }))
+        }
         var audio = new Audio()
         audio.src = "earn-point.wav"
         audio.play()
-        
-        setTimeout(() => {
-          score += enemy.point
-          console.log(`(+) = ${enemy.point} score`)
+        if (enemy.radius - 10 > 10) {
+          projectiles.splice(projectileIndex, 1)
+          enemy.radius -= 10
+          score += 50
+        } else {
           enemies.splice(index, 1)
           projectiles.splice(projectileIndex, 1)
-        },0)
+          setTimeout(() => {
+            score += enemy.point
+            console.log(projectiles.length)
+           // console.log(enemies.length)
+            //console.log(`(+) = ${enemy.point} score`)
+          },0)
+        }
       }
     });
   })
 }
 function yes(){
-  window.location.reload()
+  popUp.style.display="none"
+  scoreboard.style.display="block"
+  clearInterval(ending)
+  clearInterval(opening)
+  init()
 }
 function no(){
   localStorage.setItem("lastScore",0)
   let body = document.querySelector("body")
       let element = `
-      <div class="game-over-popUp">
-      <p>Game Over</p>
-      <p>Your Score : ${score}</p>
+      <div class="popUp">
+      <h1>${score}</h1>
+      <p>score</p>
+      <button onclick="yes()" >play</button>
       </div>`;
+      
       body.insertAdjacentHTML("beforeend", element)
 }
+opening = setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
+    let x,y
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+    const angle = Math.atan2(
+    canvas.height / 2 - y,
+    canvas.width / 2 - x
+    )
+    //const speed = Math.floor(Math.random() * (5 - 3) + 3)
+    const speed = 1
+    const velocity = {
+     x: Math.cos(angle) * speed,
+     y: Math.sin(angle) * speed
+    }
+    const point = Math.floor(Math.random() * (150 - 100) + 100)
+    enemies.push(new Enemy(x,y,radius,color,velocity, point))
+  },100)
+function init(argument) {
+  addEventListener("click", (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+    )
+  //const speed = Math.floor(Math.random() * (6 - 4) + 4)
+  const speed = 5
+  const rad = 5
+  const velocity = {
+    x: Math.cos(angle) * speed,
+    y: Math.sin(angle) * speed
+  }
+  //const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+  const color = "#fff"
+  projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, rad, color, velocity)
+  )
+  
+})
+  player = new Player(x,y,10,"white")
+  projectiles = []
+  enemies = []
+  particels = []
+  animate()
+  spawnEnemies(1000)
+}
+enemies = []
 animate()
-spawnEnemies()

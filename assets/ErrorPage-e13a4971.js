ef604f290885b82d6ce44e654dfe6b253b01680a
fiandev <wongkeras2@gmail.com>
2023-05-24 19:39:02 +0700

    UI: fix font-size

--- assets/ErrorPage-e13a4971.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-2528bc00.js";function r({code:l=500,message:s="server error",suggest:t}){return e.jsxs("div",{className:"h-screen flex flex-col justify-center text-center items-center",children:[e.jsx("h1",{className:"text-main font-bold text-xl lg:text-2xl",children:l}),e.jsxs("div",{className:"font-semibold flex flex-col text-center",children:[e.jsx("p",{className:"text-gray-600 text-lg lg:text-xl",children:s}),t?e.jsx("span",{className:"text-blue-400 text-xs lg:text-sm",children:t}):null]})]})}export{r as default};
-//# sourceMappingURL=ErrorPage-e13a4971.js.map
